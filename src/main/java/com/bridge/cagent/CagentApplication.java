7f1ebb36ea81e303e102d0aca423123db06cdf53
Avijit Bhadra <codewithavijit@gmail.com>
2026-02-16 23:26:26 +0530

    add backend

---
+++ src/main/java/com/bridge/cagent/CagentApplication.java
@@ -0,0 +1,13 @@
+package com.bridge.cagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CagentApplication.class, args);
+	}
+
+}
